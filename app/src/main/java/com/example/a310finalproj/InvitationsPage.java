bd415cc328002e0fd26ca911d48db4184fa51d99
Charlie Hill <charliea@usc.edu>
2022-11-05 11:15:56 -0700

    Non-DB class implementations

---
+++ app/src/main/java/com/example/a310finalproj/InvitationsPage.java
@@ -0,0 +1,19 @@
+package com.example.a310finalproj;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InvitationsPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // set activity xml page
+        // setContentView(R.layout.activity_);
+
+        // read non-user invitations from database
+        // add code when session implemented to filter out user-created invitations
+
+    }
+}
