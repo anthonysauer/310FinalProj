f7363e8e4343541b9e14c366ee6083d1af17d4fb
Charlie Hill <charliea@usc.edu>
2022-11-06 21:15:45 -0800

    Incomplete implementation of invitation and invitations page due to coupling

--- app/src/main/java/com/example/a310finalproj/InvitationsPage.java
+++ app/src/main/java/com/example/a310finalproj/InvitationsPage.java
@@ -1,19 +1,122 @@
 package com.example.a310finalproj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InvitationsPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // set activity xml page
-        // setContentView(R.layout.activity_);
+        // TODO - assign activity xml page
+        // setContentView(R.layout.<XML FILE>);
+
+
+        //DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+        //Map<String,Object> ex = new HashMap<>();
+
+        //ex.put("/Invitation/test/id","none");
+        //ex.put("/Invitation/test7/test","test");
+
+
+
+        //db.updateChildren(ex);
 
         // read non-user invitations from database
         // add code when session implemented to filter out user-created invitations
+        FirebaseDatabase root = FirebaseDatabase.getInstance();
+        DatabaseReference invitationRef = root.getReference("Invitation");
+        invitationRef
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        //TODO-render Invitations into display
+                        ArrayList<Invitation> invitations = new ArrayList<>();
+
+                        // PULL ALL INVITATION INFO FROM DB
+
+
+                        //user id
+                        //inv id
+                        //biography
+                        // address
+                        // rent double
+                        // utilities double
+                        // bedrooms
+                        // beds
+                        // bathrooms
+                        // pets bool
+                        // deadline Date
+
+                        // ITERATE THROUGH ALL INVITATION ID's
+                        for(DataSnapshot ID : snapshot.getChildren()){
+
+                            // ITERATE THROUGH DATA MEMBERS AND ADD TO NEW INVITATION
+                            Invitation inv = new Invitation();
+                            for(DataSnapshot dataMember : ID.getChildren()){
+                                switch(dataMember.getKey()) {
+                                    case "biography":
+                                        inv.setBiography(dataMember.getValue().toString());
+                                        break;
+                                    case "address":
+                                        inv.setAddress(dataMember.getValue().toString());
+                                        break;
+                                    case "rent":
+                                        String tempRent = dataMember.getValue().toString();
+                                        Double rent = Double.parseDouble(tempRent);
+                                        inv.setRent(rent);
+                                        break;
+                                    case "utilities":
+                                        String tempUtil = dataMember.getValue().toString();
+                                        Double utilities = Double.parseDouble(tempUtil);
+                                        inv.setUtilities(utilities);
+                                        break;
+                                    case "bedrooms":
+                                        String tempBeds = dataMember.getValue().toString();
+                                        int beds = Integer.parseInt(tempBeds);
+                                        inv.setBeds(beds);
+                                        break;
+                                    case "bathrooms":
+                                        String tempBath = dataMember.getValue().toString();
+                                        int baths = Integer.parseInt(tempBath);
+                                        inv.setBathrooms(baths);
+                                        break;
+                                    case "pets":
+                                        String tempPets = dataMember.getValue().toString();
+                                        Boolean pets = Boolean.valueOf(tempPets);
+                                        inv.setPets(pets);
+                                        break;
+                                    case "deadline":
+                                        String tempDate = dataMember.getValue().toString();
+
+                                        break;
+                                }
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.w("firebase", "loadPost:onCancelled", error.toException());
+                    }
+                });
+
 
     }
 }
